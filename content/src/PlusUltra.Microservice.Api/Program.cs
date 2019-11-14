3d0a3bfd263fc1fb1e757d86e04f787297b7625e
Henrique <Henrique@DESKTOP-QMINP2S>
2019-11-13 23:11:41 -0300

    correcoes e melhorias

--- content/src/PlusUltra.Microservice.Api/Program.cs
+++ content/src/PlusUltra.Microservice.Api/Program.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using App.Metrics;
 using App.Metrics.AspNetCore;
 using App.Metrics.Formatters.Prometheus;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
-namespace PlusUltra.Microservice
+namespace PlusUltra.Microservice.Api
 {
     public class Program
     {
